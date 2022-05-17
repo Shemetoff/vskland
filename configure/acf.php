88d7c4719397b8fe08aa292f9d79d3aca2316777
Shemetoff <52326988+Shemetoff@users.noreply.github.com>
2022-05-17 11:30:29 +0300

    theme settings

--- configure/acf.php
+++ configure/acf.php
@@ -1,3 +1,20 @@
 <?php
+add_action('init', 'my_acf_op_init');
+function my_acf_op_init()
+{
 
-// ACF functions here
+    // Check function exists.
+    if (function_exists('acf_add_options_page')) {
+
+        // Register options page.
+        $option_page = acf_add_options_page(array(
+            'page_title' => __('Theme General Settings'),
+            'menu_title' => __('Theme Settings'),
+            'menu_slug' => 'theme-general-settings',
+            'capability' => 'edit_posts',
+            'redirect' => false
+        ));
+
+
+    }
+}
